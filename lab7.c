5dc8b72e5a4ce52a124e34a459d1afa032658095
luciabianca24 <luciabiancatudorache2005@gmail.com>
2024-05-28 20:21:45 +0300

    lab7

--- lab7.c
+++ lab7.c
@@ -1,6 +1,5 @@
-#include <iostream>
 #include <stdint.h>
-using namespace std;
+#include <stdio.h>
 typedef struct nod
 {
     int info;
@@ -12,7 +11,7 @@ nod *new_nod(int info, nod *next)
     if((nou=(nod*)malloc(sizeof(nod))) == NULL)
     {
         printf("out of memory");
-        exit(EXIT_FAILURE);
+        exit(-1);
     }
     nou->info = info;
     nou->next = next;
@@ -23,6 +22,7 @@ void afisare(nod *lista)
     nod *aux = NULL;
     for(aux = lista; aux != NULL; aux = aux->next)
         printf("%d ", aux->info);
+    printf("\n");
 
 }
 nod *add_end(int info, nod *lista)
@@ -138,7 +138,153 @@ void ex3()
     freeList(list2);
     freeList(lista);
 }
+void ex2()
+{
+    nod *lista = NULL;
+    int l_size = 0;
+    lista = init(&l_size);
+    nod *pre = NULL, *urm = NULL, *p = NULL;
+    for(p = lista->next, pre = lista; p ->next != NULL; pre = p, p = urm)
+    {
+        urm = p ->next;
+        p->next = pre;
+
+    }
+    p->next = pre;
+    lista->next = NULL;
+    lista = p;
+    afisare(lista);
+    freeList(lista);
+}
+void ex5()
+{
+    nod *lista1 = NULL;
+    nod *lista2 = NULL;
+    int size_lista1, size_lista2, ok = 1;
+    lista1 = init(&size_lista1);
+    lista2 = init(&size_lista2);
+    nod *aux = NULL;
+    for(aux = lista1; aux != NULL; aux = aux -> next)
+    {
+        int info = aux -> info;
+        if(isinlist (lista2, info) == 0)
+        {
+            ok = 0;
+            break;
+        }
+    }
+    for(aux = lista2; aux != NULL; aux = aux -> next)
+    {
+        int info = aux -> info;
+        if(isinlist (lista1, info) == 0)
+        {
+            ok = 0;
+             break;
+        }
+    }
+    freeList(lista1);
+    freeList(lista2);
+    if(ok == 1)
+        printf("Listele sunt la fel");
+    else
+        printf("Listele sunt diferite");
+}
+int *stergere(int x, nod *lista)
+{
+    nod *p = NULL, *pre = NULL;
+    if(lista -> info == x)
+    {
+        lista = lista -> next;
+        return lista;
+    }
+    for(pre = lista, p = lista -> next; p != NULL; pre = p, p = p -> next)
+    {
+        int info;
+        info = p -> info;
+        if(info == x)
+        {
+            pre -> next = p -> next;
+            break;
+        }
+    }
+    return lista;
+}
+void ex4()
+{
+    int x, l_size;
+    nod *lista = NULL;
+    lista = init(&l_size);
+    scanf("%d", &x);
+    lista = stergere(x, lista);
+    afisare(lista);
+    freeList(lista);
+}
+nod* inserare_crescatoare(int x, nod *lista)
+{
+    nod *pre = NULL, *p = NULL, *nou = NULL;
+    nou = new_nod(x, NULL);
+    if(lista == NULL)
+        lista = nou;
+    else
+    {
+        if(x < lista -> info)
+        {
+            p = lista;
+            lista = nou;
+            nou -> next = p;
+        }
+        else
+        {
+            for(pre = lista, p = lista -> next; p != NULL; pre = p, p = p -> next)
+            {
+                if(pre -> info <= x && p -> info >= x)
+                {
+                    pre -> next = nou;
+                    nou -> next = p;
+                    break;
+                }
+            }
+            if(p == NULL)
+                add_end(x, lista);
+        }
+    }
+        return lista;
+}
+nod *sortare(nod *lista)
+{
+    nod *lista_cresc = NULL, *aux = NULL;
+    for(aux = lista; aux != NULL; aux = aux -> next)
+    {
+        int info = aux -> info;
+        lista_cresc = inserare_crescatoare(info, lista_cresc);
+    }
+    return lista_cresc;
+}
+void ex6()
+{
+    nod *lista = NULL;
+    int list_size = 0, x = 0;
+    lista = init(&list_size);
+    //scanf("%d", &x);
+    //lista = inserare_crescatoare(x, lista);
+    lista = sortare(lista);
+    afisare(lista);
+    freeList(lista);
+}
+void ex7()
+{
+    nod *lista1 = NULL, *lista2 = NULL, *lista = NULL, *aux;
+    int l_size1, l_size2;
+    lista1 = init(&l_size1);
+    lista2 = init(&l_size2);
+    for(aux = lista1; aux != NULL; aux = aux->next)
+        lista = add_end(aux->info, lista);
+    for(aux = lista2; aux != NULL; aux = aux->next)
+        lista = add_end(aux->info, lista);
+    lista = sortare(lista);
+    afisare(lista);
+}
 int main()
 {
-    ex3();
+    ex7();
 }
